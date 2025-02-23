6f0c409622faf85d086362fb57e66952f82f3387
ich-wtag <irtiza.chowdhury@welldev.io>
2025-02-23 14:59:42 +0600

    refactor :removed key from prop

--- src/components/ui/marker.jsx
+++ src/components/ui/marker.jsx
@@ -1,10 +1,10 @@
 import { Marker } from "react-map-gl/mapbox";
 
-const MarkerComponent = ({ key, longitude, latitude, Icon, ...args }) => {
+const MarkerComponent = ({ longitude, latitude, Icon, ...args }) => {
   const { children } = args;
 
   return (
-    <Marker key={key} longitude={longitude} latitude={latitude}>
+    <Marker longitude={longitude} latitude={latitude}>
       <div>{children} </div>
       <img src={Icon} />
     </Marker>
