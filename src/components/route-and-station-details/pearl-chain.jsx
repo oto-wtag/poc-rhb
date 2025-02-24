c77768a363643a98dd200e85abed0a3fb6278c42
Orgha Tomal Roy <orgha.tomal@welldev.io>
2025-02-24 18:23:28 +0600

    feature: implemented pearl chain

--- src/components/route-and-station-details/pearl-chain.jsx
+++ src/components/route-and-station-details/pearl-chain.jsx
@@ -2,13 +2,32 @@ import React from "react";
 import Timeline from "@/components/ui/timeline";
 
 const PearlChain = ({ data }) => {
-  return (
-    <>
-      {data.routeDetail?.map((detail, index) => (
-        <Timeline key={index} time={detail.time} station={detail.station} />
-      ))}
-    </>
-  );
+  const timelineData = data.stations;
+
+  // dummy data of how to send station details
+
+  // const timelineData = [
+  //   {
+  //     id: 1,
+  //     name: "Zurich HB",
+  //     latitude: 47.378177,
+  //     longitude: 8.540192,
+  //     details: () => {
+  //       return <div>this is first item</div>;
+  //     },
+  //   },
+  //   {
+  //     id: 2,
+  //     name: "Lugano",
+  //     latitude: 46.005,
+  //     longitude: 8.9511,
+  //     details: () => {
+  //       return <div>This is second item</div>;
+  //     },
+  //   },
+  // ];
+
+  return <Timeline data={timelineData} />;
 };
 
 export default PearlChain;
--- src/components/ui/timeline.jsx
+++ src/components/ui/timeline.jsx
@@ -1,24 +1,31 @@
 import React from "react";
 
-const Timeline = ({ time, station }) => {
+const Timeline = ({ data }) => {
   return (
-    <div>
-      <div className="flex">
-        <div className="w-10">
-          <span className="text-base text-foreground">{time}</span>
-        </div>
+    <>
+      {data.map((station, index) => (
+        <div key={index}>
+          <div className="flex">
+            <div className="w-10">
+              <span className="text-base text-foreground">{station.time}</span>
+            </div>
 
-        <div className="relative last:after:hidden after:absolute after:top-5 after:bottom-0 after:start-3.5 after:w-px after:-translate-x-[0.5px] after:bg-foreground">
-          <div className="relative h-6 z-10 size-7 flex justify-center items-center">
-            <div className="size-2 rounded-full bg-foreground"></div>
+            <div className="relative last:after:hidden after:absolute after:top-0 after:bottom-0 after:start-3.5 after:w-px after:-translate-x-[0.5px] after:bg-[#A9A9A9]">
+              <div className="relative h-6 z-10 size-7 flex justify-center items-center">
+                <div className="h-3 w-3 rounded-full border border-[#A9A9A9] bg-background" />
+              </div>
+            </div>
+
+            <div className="pb-6">
+              <h3 className="text-foreground">
+                {station.name}
+                {station.details && station.details()}
+              </h3>
+            </div>
           </div>
         </div>
-
-        <div className="pb-6">
-          <h3 className="text-foreground">{station}</h3>
-        </div>
-      </div>
-    </div>
+      ))}
+    </>
   );
 };
 
