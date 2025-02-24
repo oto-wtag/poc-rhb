b658e9a5d76656cd5358a6131edf39b3a2f1cfe1
Orgha Tomal Roy <orgha.tomal@welldev.io>
2025-02-24 10:34:13 +0600

    feature: added route details sheet

--- src/components/ui/tabs.jsx
+++ src/components/ui/tabs.jsx
@@ -1,63 +1,55 @@
-import * as React from "react"
-import * as TabsPrimitive from "@radix-ui/react-tabs"
+import * as React from "react";
+import * as TabsPrimitive from "@radix-ui/react-tabs";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
-function Tabs({
-  className,
-  ...props
-}) {
+function Tabs({ className, ...props }) {
   return (
-    (<TabsPrimitive.Root
+    <TabsPrimitive.Root
       data-slot="tabs"
       className={cn("flex flex-col gap-2", className)}
-      {...props} />)
+      {...props}
+    />
   );
 }
 
-function TabsList({
-  className,
-  ...props
-}) {
+function TabsList({ className, ...props }) {
   return (
-    (<TabsPrimitive.List
+    <TabsPrimitive.List
       data-slot="tabs-list"
       className={cn(
-        "bg-muted text-muted-foreground inline-flex h-9 w-fit items-center justify-center rounded-lg p-1",
+        "inline-flex w-full items-center justify-start  gap-0 border-b-2 border-border p-0 text-muted-foreground",
         className
       )}
-      {...props} />)
+      {...props}
+    />
   );
 }
 
-function TabsTrigger({
-  className,
-  ...props
-}) {
+function TabsTrigger({ className, ...props }) {
   return (
-    (<TabsPrimitive.Trigger
+    <TabsPrimitive.Trigger
       data-slot="tabs-trigger"
       className={cn(
-        "data-[state=active]:bg-background data-[state=active]:text-foreground ring-ring/10 dark:ring-ring/20 dark:outline-ring/40 outline-ring/50 inline-flex items-center justify-center gap-2 rounded-md px-2 py-1 text-sm font-medium whitespace-nowrap transition-[color,box-shadow] focus-visible:ring-4 focus-visible:outline-1 disabled:pointer-events-none disabled:opacity-50 aria-invalid:focus-visible:ring-0 data-[state=active]:shadow-sm [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+        "mx-2 -mb-[2px] inline-flex items-center justify-start whitespace-nowrap border-b-2 px-2 py-2 text-base font-medium transition-all first-of-type:ml-0 disabled:pointer-events-none disabled:text-muted-foreground data-[state=active]:border-primary data-[state=inactive]:border-transparent data-[state=active]:font-semibold data-[state=active]:text-foreground",
         className
       )}
-      {...props} />)
+      {...props}
+    />
   );
 }
 
-function TabsContent({
-  className,
-  ...props
-}) {
+function TabsContent({ className, ...props }) {
   return (
-    (<TabsPrimitive.Content
+    <TabsPrimitive.Content
       data-slot="tabs-content"
       className={cn(
-        "ring-ring/10 dark:ring-ring/20 dark:outline-ring/40 outline-ring/50 flex-1 transition-[color,box-shadow] focus-visible:ring-4 focus-visible:outline-1 aria-invalid:focus-visible:ring-0",
+        "mt-2  ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
         className
       )}
-      {...props} />)
+      {...props}
+    />
   );
 }
 
-export { Tabs, TabsList, TabsTrigger, TabsContent }
+export { Tabs, TabsList, TabsTrigger, TabsContent };
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import MapComponent from "@/components/map-component";
+import { useSearchParams } from "react-router-dom";
+
+import RouteSheet from "@/components/main-sheets/route-sheet";
 
 const Home = () => {
-  return <MapComponent />;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const currentRoute = searchParams.get("r");
+  const [routesSheetOpen, setRoutesSheetOpen] = useState(false);
+  const [stationsSheetOpen, setStationsSheetOpen] = useState(false);
+  const [incidentsSheetOpen, setIncidentsSheetOpen] = useState(false);
+  const [maintenanceSheetOpen, setMaintenanceSheetOpen] = useState(false);
+
+  useEffect(() => {
+    const setSheets = () => {
+      if (currentRoute === "routes") {
+        setRoutesSheetOpen(true);
+      } else {
+        setRoutesSheetOpen(false);
+      }
+    };
+    setSheets();
+  }, [currentRoute]);
+
+  return (
+    <>
+      <RouteSheet
+        routesSheetOpen={routesSheetOpen}
+        setRoutesSheetOpen={setRoutesSheetOpen}
+      />
+      <MapComponent />
+    </>
+  );
 };
 
 export default Home;
