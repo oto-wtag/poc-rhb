a8b8e2f522a7d6ab03e4dd30a8f2d86808529680
Orgha Tomal Roy <orgha.tomal@welldev.io>
2025-02-24 18:40:14 +0600

    bug fix: fixed timeline styling

--- src/components/main-sheets/route-details-sheet.jsx
+++ src/components/main-sheets/route-details-sheet.jsx
@@ -119,9 +119,9 @@ const RouteDetailsSheet = ({
                   <span className="text-base text-primary">14:20</span>
                 </div>
 
-                <div className="relative last:after:hidden after:absolute after:top-5 after:bottom-0 after:start-3.5 after:w-px after:-translate-x-[0.5px] after:bg-primary">
+                <div className="relative last:after:hidden after:absolute after:top-0 after:bottom-0 after:start-3.5 after:w-px after:-translate-x-[0.5px] after:bg-[#A9A9A9]">
                   <div className="relative h-6 z-10 size-7 flex justify-center items-center">
-                    <div className="size-2 rounded-full bg-primary"></div>
+                    <div className="h-3 w-3 rounded-full border border-[#A9A9A9] bg-background" />
                   </div>
                 </div>
 
--- src/components/ui/timeline.jsx
+++ src/components/ui/timeline.jsx
@@ -10,7 +10,15 @@ const Timeline = ({ data }) => {
               <span className="text-base text-foreground">{station.time}</span>
             </div>
 
-            <div className="relative last:after:hidden after:absolute after:top-0 after:bottom-0 after:start-3.5 after:w-px after:-translate-x-[0.5px] after:bg-[#A9A9A9]">
+            <div
+              className={`relative ${
+                index === 0
+                  ? "last:after:hidden after:absolute after:top-0 after:-bottom-2 after:start-3.5 after:w-px after:-translate-x-[0.5px] after:bg-[#A9A9A9]"
+                  : data.length !== index + 1
+                  ? "last:after:hidden after:absolute after:top-2 after:-bottom-2 after:start-3.5 after:w-px after:-translate-x-[0.5px] after:bg-[#A9A9A9]" // Other items except last
+                  : ""
+              }`}
+            >
               <div className="relative h-6 z-10 size-7 flex justify-center items-center">
                 <div className="h-3 w-3 rounded-full border border-[#A9A9A9] bg-background" />
               </div>
