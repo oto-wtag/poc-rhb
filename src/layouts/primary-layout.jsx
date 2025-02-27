2f3b3c9a11c748d1a57b0982f596a8a8194964b6
Orgha Tomal Roy <orgha.tomal@welldev.io>
2025-02-27 11:35:28 +0600

    bug fix: handled scroll when modal is up on mobile

--- src/layouts/primary-layout.jsx
+++ src/layouts/primary-layout.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
+import { useIsMobile } from "@/hooks/use-mobile";
 
 const PrimaryLayout = () => {
+  const isMobile = useIsMobile();
+
+  useEffect(() => {
+    if (isMobile) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+
+    return () => {
+      document.body.style.overflow = ""; // Cleanup
+    };
+  }, [isMobile]);
+
   return (
     <div>
       <Navbar />
