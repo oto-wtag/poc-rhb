b8b906128db6bf84029068034fe289d707a1039b
Orgha Tomal Roy <orgha.tomal@welldev.io>
2025-03-10 17:29:44 +0600

    bug fix: fixed focus zoom issue

--- src/components/ui/input.jsx
+++ src/components/ui/input.jsx
@@ -16,7 +16,7 @@ function Input({ className, type, startIcon, endIcon, ...props }) {
       <input
         type={type}
         className={cn(
-          "flex h-10 w-full rounded-md border border-input bg-background py-2 px-4 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50",
+          "flex h-10 w-full rounded-md border border-input bg-background py-2 px-4 text-md ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50 !focus:scale-0",
           startIcon ? "pl-8" : "",
           endIcon ? "pr-8" : "",
           className
